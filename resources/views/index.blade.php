05dd22b1094a7cc420c884467a74e0a9d7a357c9
3A532067 <s3a532067@student.ncut.edu.tw>
2019-01-06 23:41:51 +0800

    後台頁面初始

---
+++ resources/views/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">
+                新增文章 <small>請輸入文章內容</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li class="active">
+                    <i class="fa fa-edit"></i> 文章管理
+                </li>
+            </ol>
+        </div>
+    </div>
+    <div class="row">
+        @for ($nun = 100000; $nun >0; $nun--)
+            111
+        @endfor
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    </div>
+
+@endsection
