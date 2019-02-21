fa56bd7e1323b95da15a3dabfdae63ab3a5c6cfc
3A532067 <s3a532067@student.ncut.edu.tw>
2019-02-22 02:26:39 +0800

    counter check頁面-初版建置

---
+++ resources/views/backstage/counter/check/index.blade.php
@@ -0,0 +1,15 @@
+@extends('backstage.counter.layouts.master')
+@section('content')
+<!-- Page Heading -->
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">
+            <font color="#000000" face="微軟正黑體">確認餐點_Check</font>
+        </h1>
+    </div>
+</div>
+
+<!-- /.row -->
+
+<!-- /.row -->
+@endsection
