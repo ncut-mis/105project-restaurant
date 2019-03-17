1eef2786fe0d39cb3f65284644262a620e2f9701
3A532081 <tanshui601@gmail.com>
2019-03-17 19:04:01 +0800

    推波test

--- resources/views/backstage/chef/layouts/master.blade.php
+++ resources/views/backstage/chef/layouts/master.blade.php
@@ -28,13 +28,20 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-
-
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <![endif]-->
 
+    <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+    <script>
+        var OneSignal = window.OneSignal || [];
+        OneSignal.push(function() {
+            OneSignal.init({
+                appId: "59ec5b4e-aa4e-4ee2-877f-edf0b9265528",
+            });
+        });
+    </script>
 </head>
 
 <body>
@@ -69,7 +76,6 @@
     <script src="{{ asset('js/plugins/morris/raphael.min.js') }}"></script>
     <script src="{{ asset('js/plugins/morris/morris.min.js') }}"></script>
     <script src="{{ asset('js/plugins/morris/morris-data.js') }}"></script>
-
 </body>
 
 </html>
