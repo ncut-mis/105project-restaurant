d2db25d2dd3b9ebfc28245f4c18344e0d886be5e
3A532067 <s3a532067@student.ncut.edu.tw>
2019-03-19 17:38:40 +0800

    新增一個資料表 order

---
+++ app/Http/Controllers/OrderTableController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OrderTable;
+use Illuminate\Http\Request;
+
+class OrderTableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\OrderTable  $orderTable
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrderTable $orderTable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\OrderTable  $orderTable
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OrderTable $orderTable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\OrderTable  $orderTable
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrderTable $orderTable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\OrderTable  $orderTable
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrderTable $orderTable)
+    {
+        //
+    }
+}
