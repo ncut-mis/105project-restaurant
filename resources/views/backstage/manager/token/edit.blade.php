b13331978d79462deaa7cf3d0cc83527063baeed
3A532059 <coco214034@gmail.com>
2019-05-06 00:41:50 +0800

    Token小修

--- resources/views/backstage/manager/token/edit.blade.php
+++ resources/views/backstage/manager/token/edit.blade.php
@@ -44,7 +44,7 @@
 <!-- /.row -->
 @include('backstage.manager.layouts.partials.validation')
 <!-- /.row -->
-
+<font color="#000000" face="微軟正黑體" style="text-align: center">
 <div class="row">
     <form action="/backstage/token/{{$restaurant->id}}" method="POST" role="form">
         {{ csrf_field() }}
@@ -53,28 +53,29 @@
             <div class="table-responsive">
                 <table class="table table-bordered table-hover" style="border:3px #9BA2AB solid;">
                     <thead style="border:2px #9BA2AB solid;">
-                    <tr style="background-color: lightgrey;">
-                        <th width="700" style="text-align: center">Token</th>
-                        <th width=50" style="text-align: center">送出</th>
-                    </tr>
+                        <tr style="background-color: lightgrey;">
+                            <th width="1000" style="text-align: center">Token</th>
+                            <th width="50" style="text-align: center">送出</th>
+                        </tr>
                     </thead>
                     <tbody style="border:3px #9BA2AB solid;">
-                    <tr>
-                        <td>
-                            <input name="token" class="form-control" placeholder="請輸入名稱" value="{{$restaurant->token}}" required>
-                        </td>
-                        <td>
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-success">更新</button>
-                            </div>
-                        </td>
-                    </tr>
+                        <tr>
+                            <td>
+                                <input name="token" class="form-control" placeholder="請輸入名稱" value="{{$restaurant->token}}" required>
+                            </td>
+                            <td>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-success">更新</button>
+                                </div>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
         </div>
     </form>
 </div>
+</font>
 
 
 
